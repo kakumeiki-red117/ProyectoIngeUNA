3992defd7ea4ce8d991dad3a8a8f602df813511b
jeanelbicho3 <jeanabarca@hotmail.com>
2016-10-09 00:37:26 -0600

    Se implementa la primera hoja de decomiso, en javascript dinamicamente
    Signed-off-by: jeanelbicho3 <jeanabarca@hotmail.com>

--- PaginaWebSGUC/js/js.js
+++ PaginaWebSGUC/js/js.js
@@ -1,82 +1,3 @@
-$(document).ready(function(){
-
-	$("#banner").css({"height":$(window).height() + "px"});
-
-	var flag = false;
-	var scroll;
-
-	$(window).scroll(function(){
-		scroll = $(window).scrollTop();
-
-		if(scroll > 200){
-			if(!flag){
-				$("#logo").css({"margin-top": "-5px", "width": "50px","height":"50px"});
-
-				$("header").css({"background-color": "#3C3C3C"});
-				flag = true;
-			}
-		}else{
-			if(flag){
-				$("#logo").css({"margin-top": "150px", "width": "250px","height":"250px"});
-
-				$("header").css({"background-color": "transparent"});
-				flag = false;
-			}
-		}
-
-
-	});
-
-});
-
-
-$(document).on("ready", inicio);
-
-function inicio(){
-	$("#container img").on("click",abrirFull);
-	$("#muestra").on("click", cerrarFull);
-};
-
-function abrirFull(){
-	//alert($(this).attr("alt"));
-	var nombre = $(this).attr("alt") + "G";
-	var direccion = "gallery/" + nombre + ".jpg";
-
-	$("#imgBIG").attr("src",direccion);
-
-	$("#muestra").fadeIn();
-};
-
-function cerrarFull(){
-	$(this).fadeOut();
-};
-
-var expr = /^[a-zA=Z0-9_\.\-]+@[a-zA=Z0-9\-]+\.[a-zA=Z0-9\-\.]+$/;
-$(document).ready(function(){
-	$("#bEnviar").click(function(){
-		var nombre = $("#itNombre").val();
-		var correo = $("#itMail").val();
-		var asunto = $("#itAsunto").val();
-
-		if(nombre == ""){
-			$("#mensaje1").fadeIn();
-			return false;
-		}else{
-			$("#mensaje1").fadeOut();
-			if(correo == "" || !expr.test(correo)){
-				$("#mensaje2").fadeIn();
-				return false;
-			}else{
-				$("#mensaje2").fadeOut();
-				if(asunto == ""){
-					$("#mensaje3").fadeIn();
-					return false;
-				}
-			}
-		}
-
-	});
-});
 
 function generate_table(value) {
   var body = document.getElementsByTagName("body")[0];
@@ -99,4 +20,50 @@ function generate_table(value) {
   tbl.appendChild(tblBody);
   body.appendChild(tbl);
   tbl.setAttribute("border", "2");
+}
+
+function generate_tableRow(tableName, value) {
+
+
+  var row = document.createElement("th");
+  row.style.backgroundColor = "blue";
+  row.style.color = "white";
+  row.style.textAlign = "center";
+  row.style.width = "500px";
+  row.colSpan = "6";
+  //var tittle = document.createTextNode(value);
+  row.innerHTML = value;
+  //row.appendChild(tittle);
+
+  tableName.appendChild(row);
+  tableName.setAttribute("border", "2");
+}
+
+function generate_tableCell(tableName,row,value,isBlue,isBox) {
+  //var body = document.getElementsByTagName("body")[0];
+
+
+      var cell = document.createElement("td");
+
+      var cellText = document.createTextNode(value);
+      var cellInput = value;
+
+			(isBlue)?(
+				        cell.style.backgroundColor = "blue"
+                ,cell.style.color = "white"
+              )
+			        :
+                cell.style.backgroundColor = "white";
+
+      (isBox)?
+                cell.appendChild(value)
+              :
+                cell.appendChild(cellText);
+
+
+
+  row.appendChild(cell);
+  tableName.appendChild(row);
+  //body.appendChild(tableName);
+  tableName.setAttribute("border", "2");
 }
